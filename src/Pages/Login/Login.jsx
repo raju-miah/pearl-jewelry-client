f2fe6912e10db9c29561d78e2ed008b2315d7cae
raju-miah <rajumiahpollob7@gmail.com>
2023-10-04 02:37:15 +0600

    added login page and divider component

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,56 +1,75 @@
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom';
+import arrowBtnImgLogin from '../../assets/arrow.svg';
+import Divider from '../../components/Divider/Divider';
+import { BsGoogle } from 'react-icons/bs';
+import { FaFacebook } from 'react-icons/fa6';
 
 const Login = () => {
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-lg max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-6">Welcome Back!</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">Email Address</label>
-            <input
-              type="email"
-              className="mt-1 p-2 w-full border rounded-md"
-              placeholder="john@example.com"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">Password</label>
-            <input
-              type="password"
-              className="mt-1 p-2 w-full border rounded-md"
-              placeholder="********"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-indigo-600 text-white p-2 rounded hover:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200"
-          >
-            Login
-          </button>
-        </form>
-        <div className="mt-4 text-center">
-          <span>Don&apos;t have an account? </span>
-          <Link to="/register" className="text-indigo-600 hover:underline">
-            Register here
-          </Link>
-        </div>
+  return (
+    <section className='pt-12 pb-12'>
+      <div>
+        <h2 className="text-[60px] text-[#153462] italic text-center">Log in</h2>
+      </div>
 
-        <div className="mt-4 text-center">
-          <span>Or log in with</span>
-          <button
-            type="submit"
-            className="w-full bg-indigo-600 text-white p-2 rounded hover:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200"
-          >
-            Google
-          </button>
+      <div>
+        <div 
+          className="max-w-[737px] flex flex-col mx-auto rounded-[40px]">
+
+          <form className="py-[30px] px-[125px]">
+              <input
+                className="w-full text-[#153462] text-[22px] bg-transparent border-0 border-b-[1px] border-[#153462] outline-none py-[15px]"
+                type="email" name="email" placeholder="Email"/>
+
+              <input
+               className="w-full text-[#153462] text-[22px] bg-transparent border-0 border-b-[1px] border-[#153462] outline-none py-[15px]"
+               type="password" name="password" placeholder="Password"/>
+
+                <div className='flex justify-between items-center my-[30px] gap-[30px]'>
+                  <label 
+                  className='flex items-center gap-[10px] text-[22px] cursor-pointer text-[#153462]'>
+                    <input type="checkbox" name="check" />
+                    <span>Remember me</span>
+                  </label>
+
+                  <Link className='text-[22px] text-[#153462]'>Forgot Password?</Link>
+                </div>
+
+                <div className='flex items-center gap-[20px] my-[30px]'>
+                  <input 
+                   className='text-[32px] text-[#153462] cursor-pointer'
+                   type="submit" value="Log in" />
+
+                   <span className='cursor-pointer'>
+                      <img
+                       src={arrowBtnImgLogin} alt="" />
+                   </span>
+               </div>
+
+               <p className='text-[22px] text-[#153462] pb-[15px]'>Don’t have an account?  
+                <Link className='underline pl-[5px]'>
+                Register Now
+                </Link>
+                </p>
+
+                <Divider></Divider>
+
+                <div className='flex justify-center items-center gap-[20px]'>
+                  <button 
+                  className='flex items-center gap-[10px] text-[22px] border-[1px] cursor-pointer text-[#153462] border-solid border-[#153462] px-[50px] py-4 mt-4 hover:bg-[#153462] hover:text-white ease-in-out duration-500'>
+                    <span className='text-[22px] text-center'><BsGoogle /></span>
+                   Google</button>
+
+                  <button 
+                  className='flex items-center gap-[10px] text-[22px] border-[1px] cursor-pointer text-[#153462] border-solid border-[#153462] px-[50px] py-4 mt-4 hover:bg-[#153462] hover:text-white ease-in-out duration-500'>
+                    <span className='text-[22px] text-center'><FaFacebook /></span>
+                   Facebook</button>
+                </div>
+
+          </form>
         </div>
       </div>
-    </div>
-    );
+    </section>
+  );
 };
 
 export default Login;
---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,13 @@
+
+
+const Divider = () => {
+    return (
+        <div className="flex items-center justify-center py-2">
+          <div className="border-t border-[#153462] flex-grow"></div>
+          <div className="mx-4 text-[22px] text-[#153462] font-bold">OR</div>
+          <div className="border-t border-[#153462] flex-grow"></div>
+        </div>
+    );
+};
+
+export default Divider;
